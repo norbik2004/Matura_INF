e9ab3acfbf902381147ecfa6b87d333813c61121
Norbert_Kieler <128638079+norbik2004@users.noreply.github.com>
2024-04-04 20:51:33 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-# elo
+data = []
 
-#EE
+
+with open("bin.txt", "r") as file:
+    for line in file:
+        data.append(line.strip())
+
+def count_blocks(number):
+    block_number = 0
+    prev_char = ''
+    for char in number:
+        if block_number == 0:
+            block_number = 1
+            prev_char = char
+        else:
+            if prev_char != char:
+                block_number += 1
+                prev_char = char
+    return block_number
+
+
+final_answer = 0
+for number in data:
+    answer = count_blocks(number)
+    if answer <= 2:
+        final_answer += 1
+print(final_answer)
