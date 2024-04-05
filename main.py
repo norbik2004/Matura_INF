590ed241c31c0b02025119985a4c388e1c4d6e6e
Norbert_Kieler <128638079+norbik2004@users.noreply.github.com>
2024-04-05 20:28:32 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with open("bin.txt", "r") as file:
         data.append(line.strip())
 
 def count_blocks(number):
-    block_number = 0
+    block_number = 1
     prev_char = ''
     for char in number:
         if block_number == 0:
@@ -25,3 +25,21 @@ for number in data:
     if answer <= 2:
         final_answer += 1
 print(final_answer)
+
+format(int(data[0], 2), "d")
+
+
+def get_highest_block(data_array):
+    highest_block = 0
+    for integer in data_array:
+        integer = int(integer, 2)
+        if integer > highest_block:
+            highest_block = integer
+    return bin(highest_block)[2:]
+
+
+print(get_highest_block(data))
+
+numerek = "1111011"
+numerek = format(int(numerek, 2), "d")
+print(numerek)
